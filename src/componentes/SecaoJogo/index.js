c9086a2573fa88697b3326492fb5d89f4e1ca71b
AshileySabah <ashileysabrina2002@gmail.com>
2021-11-02 19:06:34 -0300

    cards-configurados-consumo-ok

--- src/componentes/SecaoJogo/index.js
+++ src/componentes/SecaoJogo/index.js
@@ -2,43 +2,80 @@ import React from 'react';
 import './style.css';
 
 import Card from '../Card/index.js';
-import Botao from '../Botao/index.js';
 
 function SecaoJogo() {
+  function botaoJogar(){
+    const areaCards = document.querySelector('[data-area-cards]');
+    const btnJogar = document.querySelector('[data-button-jogar]');
+    btnJogar.textContent = 'Vai';
+    
+    if(areaCards.classList.contains('esconder')){
+      areaCards.classList.remove('esconder');
+    }
+  }
+
+  const requisicao = (url) => {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", url, false);
+    xhttp.send();
+    const resultado = JSON.parse(xhttp.responseText);
+
+    return resultado;
+  }
+
   const numeroAleatorio = (min, max) => {
     const numero = Math.floor(Math.random() * (max - min) + min);
     return numero;
   }
 
-  const montarImagemCard = () => {
-    const numeroPokemon = numeroAleatorio(1, 20);
+  const propriedadesCard = () => {
+    const numeroPokemon = numeroAleatorio(1, 10);
+
     const imagem = <img src={`https://cdn.traction.one/pokedex/pokemon/${numeroPokemon}.png`} alt="Imagem de Pokemon" className="img-caixa-card"/>
-    return imagem;
+    
+    const propriedades = [];
+    propriedades['imagem'] = imagem;
+
+    const url = `https://pokeapi.co/api/v2/pokemon/${numeroPokemon}`;
+    const dadosRequisicao = requisicao(url);
+
+    propriedades['nome'] = dadosRequisicao.forms[0].name;
+    propriedades['tipo'] = dadosRequisicao.types[0].type.name;
+    propriedades['peso'] = dadosRequisicao.weight;
+
+    return propriedades;
   }
+
+  const card1 = propriedadesCard();
+  const card2 = propriedadesCard();
+  const card3 = propriedadesCard();
 
   return (
     <section className="secao-jogar">
+      <h2 className="titulo-secundario">Boa sorte!</h2>
       <div className="area-cards esconder" data-area-cards>
         <Card
-          imagem={montarImagemCard()}
-          nome="Bulba"
-          tipo="agua"
-          peso="200"
+          imagem={card1.imagem}
+          nome={card1.nome}
+          tipo={card1.tipo}
+          peso={card1.peso}
         />
         <Card
-          imagem={montarImagemCard()}
-          nome="Bulba"
-          tipo="agua"
-          peso="200"
+          imagem={card2.imagem}
+          nome={card2.nome}
+          tipo={card2.tipo}
+          peso={card2.peso}
         />
         <Card
-          imagem={montarImagemCard()}
-          nome="Bulba"
-          tipo="agua"
-          peso="200"
+          imagem={card3.imagem}
+          nome={card3.nome}
+          tipo={card3.tipo}
+          peso={card3.peso}
         />
       </div>
-      <Botao/>
+      <button className="botao-vai" onClick={botaoJogar} data-button-jogar>
+        Começar
+      </button>
     </section>
   );
 }
